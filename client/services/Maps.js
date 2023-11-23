ce55f2fcc6b77a4882579360edc720b69183f6d7
ViFigueiredo <vifigcode@gmail.com>
2023-11-23 14:14:34 -0300

    funcoes Maps separadas

--- client/services/Maps.js
+++ client/services/Maps.js
@@ -1,7 +1,12 @@
 import { Loader } from '@googlemaps/js-api-loader'
 
 export const inicializarMapa = async (lat, lng, apiKey) => {
-  // New Map
+  const map = await criarMapa(lat, lng, apiKey)
+  const marker = adicionarMarker(map, lat, lng)
+  adicionarInfoWindow(marker, map)
+}
+
+const criarMapa = async (lat, lng, apiKey) => {
   const loader = new Loader({
     apiKey,
     version: 'weekly'
@@ -15,16 +20,18 @@ export const inicializarMapa = async (lat, lng, apiKey) => {
 
   loader.load()
   const { Map } = await google.maps.importLibrary('maps')
-  const map = new Map(document.getElementById('map'), options)
+  return new Map(document.getElementById('map'), options)
+}
 
-  // Marker
-  const marker = new google.maps.Marker({
+const adicionarMarker = (map, lat, lng) => {
+  return new google.maps.Marker({
     position: { lat, lng },
     map,
     icon: 'https://res.cloudinary.com/dkwnozyux/image/upload/v1700684417/placeholder_zrrlht.png'
   })
+}
 
-  // Info Window
+const adicionarInfoWindow = (marker, map) => {
   const detailWindow = new google.maps.InfoWindow({
     content: '<h2> Avantti Consultoria</h2>'
   })
