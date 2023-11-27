07db59edcf0ec055588389339ce625f3e6536585
ViFigueiredo <vifigcode@gmail.com>
2023-11-27 15:12:32 -0300

    alterado alinhamento da navbar

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -5,61 +5,62 @@
         <img src="@/assets/img/jnx_logo.png" class="jnx-logo h-10" alt="">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">JNX App</span>
       </a>
-      <div id="dropMenuBase" class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <button
-          id="user-menu-button"
-          type="button"
-          class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-          aria-expanded="false"
-          data-dropdown-toggle="user-dropdown"
-          data-dropdown-placement="bottom"
-          @click="showDropdown"
-        >
-          <img class="w-8 h-8 rounded-full" :src="imgUser" alt="user photo">
-        </button>
-        <!-- Dropdown menu -->
-        <div v-if="dropMenu" id="user-dropdown" class=" absolute top-11 right-0 z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
-          <div class="px-4 py-3">
-            <span class="block text-sm text-gray-900 dark:text-white">{{ userName }}</span>
-            <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ userEmail }}</span>
-          </div>
-          <ul class="py-2" aria-labelledby="user-menu-button">
-            <li>
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Configurações</a>
+
+      <div class="nav-wrapper flex">
+        <div id="navbar-user" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 mr-10">
+          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li :class="{ 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500': $route.path === '/' }">
+              <nuxt-link to="/">
+                Check-in
+              </nuxt-link>
             </li>
-            <li class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-              <nuxt-link to="/auth">
-                Sair
+            <li :class="{ 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500': $route.path === '/servicos' }">
+              <nuxt-link to="/servicos">
+                Serviços
+              </nuxt-link>
+            </li>
+            <li :class="{ 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500': $route.path === '/dashboard' }">
+              <nuxt-link to="/dashboard">
+                Dashboard
               </nuxt-link>
             </li>
           </ul>
         </div>
-        <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
-        </button>
-      </div>
-
-      <div id="navbar-user" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li :class="{ 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500': $route.path === '/' }">
-            <nuxt-link to="/">
-              Check-in
-            </nuxt-link>
-          </li>
-          <li :class="{ 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500': $route.path === '/servicos' }">
-            <nuxt-link to="/servicos">
-              Serviços
-            </nuxt-link>
-          </li>
-          <li :class="{ 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500': $route.path === '/dashboard' }">
-            <nuxt-link to="/dashboard">
-              Dashboard
-            </nuxt-link>
-          </li>
-        </ul>
+        <div id="dropMenuBase" class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            id="user-menu-button"
+            type="button"
+            class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            aria-expanded="false"
+            data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="bottom"
+            @click="showDropdown"
+          >
+            <img class="w-8 h-8 rounded-full" :src="imgUser" alt="user photo">
+          </button>
+          <div v-if="dropMenu" id="user-dropdown" class=" absolute top-11 right-0 z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+            <div class="px-4 py-3">
+              <span class="block text-sm text-gray-900 dark:text-white">{{ userName }}</span>
+              <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ userEmail }}</span>
+            </div>
+            <ul class="py-2" aria-labelledby="user-menu-button">
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Configurações</a>
+              </li>
+              <li class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                <nuxt-link to="/auth">
+                  Sair
+                </nuxt-link>
+              </li>
+            </ul>
+          </div>
+          <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   </nav>
