40e586e52eeab0ac1e76aa238cc820844d9dda03
ViFigueiredo <vifigcode@gmail.com>
2023-11-28 14:51:36 -0300

    favicon

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   runtimeConfig: {
     public: {
       GOOGLE_MAPS_KEY: process.env.GOOGLE_MAPS_KEY
